b9180ec8cd125eada1fc576e02cd44e8a681a7c3
qwertys318 <343604@gmail.com>
2020-10-23 21:00:38 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import freeton from "freeton";
+import freeton from "../freeton_dev/src/index";
+import ContractFactory from "../freeton_dev/src/contract/ContractFactory";
 
-const kington = require('../contracts/kington.json');
+const Kington = require('../contracts/Kington.json');
+const KingtonOrder = require('../contracts/KingtonOrder.json');
 
 const _ = {
   checkExtensionAvailability() {
@@ -22,7 +24,23 @@ window.app = {
   getMessages() {
     _.checkExtensionAvailability();
     const provider = _.getProvider();
-    const contract = new freeton.Contract(kington, provider);
-    contract.getMessages().get().then((messages) => console.log(messages));
+    const contract = new freeton.Contract(Kington.networks['2'].address, Kington.abi, provider);
+    contract.functions.getMessages().then((messages) => console.log(messages));
+  },
+  async deploy() {
+    _.checkExtensionAvailability();
+    const provider = _.getProvider();
+    const signer = provider.getSigner();
+    const options = {initAmount: '1000022000', initParams: {}};
+    const contractFactory = new ContractFactory(KingtonOrder.abi, KingtonOrder.imageBase64, signer, options);
+    const constructorParams = {
+      destinationAddress: Kington.networks['2'].address,
+      message: freeton.utils.stringToHex('London is the capital of Great Britain.'),
+    };
+    const contract = await contractFactory.deploy(constructorParams);
+    contract
+      .getDeployProcessing()
+      .wait()
+      .then(() => console.log(`Deployed. TxId: ${contract.getDeployProcessing().txid}`));
   }
 };
